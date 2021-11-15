daf051d4fa35525b7dd5914ccd01f18f299c8a41
Jeremy A Gray <jeremy.a.gray@gmail.com>
2021-11-14 22:23:37 -0600

    feat(cli): add initial cli entry point
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ bin/elective.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""Elective configuration generator front end."""
+
+import sys
+
+sys.path.insert(0, "/home/gray/src/work/elective")
+
+import elective  # noqa: E402
+
+
+def main(argv=None):
+    """Generate a new configurator."""
+    try:
+        conf = elective.load(argv)
+        elective.generate(conf)
+        sys.exit(0)
+    except Exception as error:
+        print(error, file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+
+    main()
