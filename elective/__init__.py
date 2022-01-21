5a28aa3e4f704c627a4e17c7332ddcf56ec4482f
Jeremy A Gray <jeremy.a.gray@gmail.com>
2022-01-21 13:55:02 -0600

    feat(mod): deprecate string code generation
    Deprecate string based code generation in favor of in-memory code
    generation.
    
    Closes: closes #1
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- elective/__init__.py
+++ elective/__init__.py
@@ -19,10 +19,3 @@ from .env import process_integer
 from .env import process_list
 from .env import process_string
 from .files import _load_toml_file
-from .generate import _generate_argparse_boolean
-from .generate import _generate_argparse_boolean_group
-from .generate import _generate_argparse_display_action
-from .generate import _generate_argparse_parser
-from .generate import _generate_file_banner
-from .generate import _generate_loader
-from .generate import generate
